c40662b70aef3f98de727910a0d02c612190253f
ism48103568 <ism48103568@a16.informatica.escoladeltreball.org>
2018-01-25 13:37:34 +0100

    upload

---
+++ enteroono.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+print "ENTEROONO"
+dividendo= input ("diga el dividendo:")
+divisor= input ("diga el divisor: ")
+
+
+if dividendo%divisor == 0: 
+	print "es entero"
+else:
+	print "no es entero"
+
+
+
+
+
+
+
+
+
+
+
