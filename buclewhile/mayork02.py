557870db0c6058adb23242d03ff5ad73c809567a
ism48103568 <ism48103568@a16.informatica.escoladeltreball.org>
2018-01-29 13:31:44 +0100

    upload

--- buclewhile/mayork02.py
+++ buclewhile/mayork02.py
@@ -1,1 +1,10 @@
-#coding: uft-8
+# coding: utf-8
+
+uno = float (input ("escriba un numero:"))
+dos = float (input ("escribe un numero mayor:"))
+
+while (uno<dos):
+	dos = float (input ("buclemagico:..."))
+print "programa terminado ",uno, dos
+
+
