a0d3b88f6fa51c76299babd252ab2b1cbc2e79b0
ism48103568 <ism48103568@a16.informatica.escoladeltreball.org>
2018-01-29 12:59:34 +0100

    upload

---
+++ buclewhile/bucle.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+uno = input ("escriba un numero:")
+dos = input ("escribe un numero mayor:")
+
+while (uno>dos):
+	dos = input ("prueba otra vez:...")
+
+print "los numeros an sido ",uno,"y",dos
+
